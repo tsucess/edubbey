2c7ec99b05af293cb1d421ab50c3c4f0edb12017
OGUNSANYA TAOFEEQ ABIDEMI <taofeeq.muhammad22@gmail.com>
2023-09-18 09:44:54 +0100

    Update constant.php
    file updated

--- subscription-system/config/constant.php
+++ subscription-system/config/constant.php
@@ -2,8 +2,8 @@
 session_start();
 define("ROOT_URL", "http://localhost/edubbey/subscription-system/");
 define("DB_HOST", "localhost");
-define("DB_USER", 'success');
-define("DB_PASS", "Taofeeq1993@");
-define("DB_NAME", "sub_system");
+define("DB_USER", '');
+define("DB_PASS", "");
+define("DB_NAME", "");
 
 ?>
